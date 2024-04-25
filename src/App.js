ffcd9e8dcc00888f946de94f650845d6e17717e5
dansbands <dansbands@hotmail.com>
2024-04-25 19:27:14 -0400

    remove debugging data

--- src/App.js
+++ src/App.js
@@ -8,11 +8,6 @@ function App() {
         <div className="App-title">A.I. To-Do List</div>
       </header>
       <div className="App-body">
-        <h3>Server: {process.env.REACT_APP_SERVER_URL}</h3>
-        <h3>Prod Server: {process.env.REACT_APP_PROD_SERVER_URL}</h3>
-        <h3>Node Env: {process.env.NODE_ENV}</h3>
-        <h3>Version: 4</h3>
-
         <Tasks />
       </div>
     </div>
