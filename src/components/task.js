1a90f647865f07f45d6d73101e6eb767c4a514be
dansbands <dansbands@hotmail.com>
2024-04-29 17:19:39 -0400

    hide delete button

--- src/components/task.js
+++ src/components/task.js
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
-import { faChevronRight, faTrash } from "@fortawesome/free-solid-svg-icons";
+import {
+  faChevronRight,
+  faEllipsis,
+  faTrash,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Chat from "./chat";
 
 const Task = ({ data, toggleCompleted, deleteTodo }) => {
-  const [isOpen, toggleOpen] = useState(false);
+  const [isControlsOpen, toggleControlsOpen] = useState(false);
+  const [isPanelOpen, togglePanelOpen] = useState(false);
 
   const { title, completed, id, _id } = data;
 
@@ -22,17 +27,30 @@ const Task = ({ data, toggleCompleted, deleteTodo }) => {
           </label>
           <div className="task-title">{title}</div>
         </div>
-        <button className="task-delete-button" onClick={() => deleteTodo(_id)}>
-          <FontAwesomeIcon icon={faTrash} />
-        </button>
-        <button
-          className={`task-toggle-button ${isOpen && "rotate-90-right"}`}
-          onClick={() => toggleOpen((prevState) => !prevState)}
-        >
-          <FontAwesomeIcon icon={faChevronRight} />
-        </button>
+        <div className="task-button-container">
+          <button
+            className="task-controls-button"
+            onClick={() => toggleControlsOpen((prevState) => !prevState)}
+          >
+            <FontAwesomeIcon icon={faEllipsis} />
+          </button>
+          <button
+            className={`task-toggle-button ${isPanelOpen && "rotate-90-right"}`}
+            onClick={() => togglePanelOpen((prevState) => !prevState)}
+          >
+            <FontAwesomeIcon icon={faChevronRight} />
+          </button>
+        </div>
+        {isControlsOpen && (
+          <button
+            className="task-delete-button"
+            onClick={() => deleteTodo(_id)}
+          >
+            <FontAwesomeIcon icon={faTrash} />
+          </button>
+        )}
       </div>
-      <div className={isOpen ? "task-info-pane" : "hidden"}>
+      <div className={isPanelOpen ? "task-info-pane" : "hidden"}>
         <Chat title={title} />
       </div>
     </>
