2de32e11421a28d8234e58de35810003a1cd2c9a
dansbands <dansbands@hotmail.com>
2024-04-29 23:15:17 -0400

    datk mode

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./App.css";
 import Tasks from "./components/tasks";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 function App() {
+  const [isLight, setIsLight] = useState(true);
+  const modeIcon = isLight ? faSun : faMoon;
+
+  const switchColorMode = () => {
+    setIsLight(prevState => !prevState);
+    const rootElement = document.body;
+    console.log('rootElement', rootElement)
+    rootElement.classList.toggle("dark-mode")
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -12,6 +25,9 @@ function App() {
       <div className="App-body">
         <Tasks />
       </div>
+      <button className="color-mode-switch" onClick={switchColorMode}>
+        <FontAwesomeIcon icon={modeIcon} />
+      </button>
     </div>
   );
 }
