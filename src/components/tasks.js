984f9880fcc6eb5bc5961cc83ccf6bd1d8951481
dansbands <dansbands@hotmail.com>
2024-05-08 21:33:37 -0400

    add loader

--- src/components/tasks.js
+++ src/components/tasks.js
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import Task from "./task";
 import { completeTodo, deleteTodo, getTodos, postTodo } from "../util/fetch";
+import loadingGif from '../img/loading.gif';
 
 const Tasks = () => {
   const [pageLoadingState, setPageLoadingState] = useState(null);
@@ -59,47 +60,50 @@ const Tasks = () => {
 
   return (
     <>
-      {pageLoadingState && (
+      {(pageLoadingState && pageLoadingState === "loading") ? (
         <div className="loading-indicator">
-          {pageLoadingState === "loading" ? "Loading resources..." : "Ready"}
+          <img src={loadingGif} alt="loading-image" />
         </div>
-      )}
-      <div className="task-form">
-        <div className="task-form-left">
-          <input
-            name="task-name"
-            value={inputValue}
-            onChange={(e) => handleInputChange(e.target.value)}
-            onKeyUp={checkForSubmit}
-          />
-          {inputError && (
-            <div className="task-input-error">
-              Please enter a task name in the input field above.
-            </div>
-          )}
-        </div>
-        <div className="task-form-right">
-          <button type="submit" onClick={handleSubmit}>
-            <FontAwesomeIcon icon={faPlus} />
-          </button>
-        </div>
-      </div>
-
-      <div className="task-list">
-        {allTasks
-          .sort((a, b) => sortByCompleted(a, b))
-          .map((taskData) => {
-            const { _id } = taskData;
-            return (
-              <Task
-                key={_id}
-                data={taskData}
-                toggleCompleted={toggleCompleted}
-                deleteTodo={() => deleteTodo(_id, allTasks, setAllTasks)}
+      ) : (
+        <>
+          <div className="task-form">
+            <div className="task-form-left">
+              <input
+                name="task-name"
+                value={inputValue}
+                onChange={(e) => handleInputChange(e.target.value)}
+                onKeyUp={checkForSubmit}
               />
-            );
-          })}
-      </div>
+              {inputError && (
+                <div className="task-input-error">
+                  Please enter a task name in the input field above.
+                </div>
+              )}
+            </div>
+            <div className="task-form-right">
+              <button type="submit" onClick={handleSubmit}>
+                <FontAwesomeIcon icon={faPlus} />
+              </button>
+            </div>
+          </div>
+
+          <div className="task-list">
+            {allTasks
+              .sort((a, b) => sortByCompleted(a, b))
+              .map((taskData) => {
+                const { _id } = taskData;
+                return (
+                  <Task
+                    key={_id}
+                    data={taskData}
+                    toggleCompleted={toggleCompleted}
+                    deleteTodo={() => deleteTodo(_id, allTasks, setAllTasks)}
+                  />
+                );
+              })}
+          </div>
+        </>
+      )}
     </>
   );
 };
