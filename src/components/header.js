7f93ace9d82a720567f5ca7297dfdb34de474b52
dansbands <danodeawebdev@gmail.com>
2025-05-19 10:26:17 -0400

    update media width and version

--- src/components/header.js
+++ src/components/header.js
@@ -5,38 +5,41 @@ import { useAuth } from "./auth";
 import LogoLight from "../img/logo-light.jpeg";
 import LogoDark from "../img/logo-dark.jpeg";
 
-const Header = ({isLight}) => {
+const Header = ({ isLight }) => {
   const auth = useAuth();
   const navigate = useNavigate();
 
   return (
-    <header className="App-header">
-      <div className="App-header-main">
-        <div className="App-title-container">
-          <img src={isLight ? LogoLight : LogoDark} alt="Logo" />
+    <>
+      <header className="App-header">
+        <div className="App-header-main">
+          <div className="App-title-container">
+            <img src={isLight ? LogoLight : LogoDark} alt="Logo" />
+          </div>
         </div>
-      </div>
-      <div className="App-header-right">
-        {auth.user ? (
-          <Link className="App-login" to="/sign-in">
-            <button
-              className="App-user"
-              onClick={() => {
-                auth.signOut(() => navigate("/sign-in"));
-              }}
-            >
-              <FontAwesomeIcon icon={faUser} />
-              {auth.user.firstName}
-            </button>
-          </Link>
-        ) : (
-          <Link className="App-login" to="/sign-in">
-            <FontAwesomeIcon icon={faRightToBracket} />
-            Log In
-          </Link>
-        )}
-      </div>
-    </header>
+        <div className="App-header-right">
+          {auth.user ? (
+            <Link className="App-login" to="/sign-in">
+              <button
+                className="App-user"
+                onClick={() => {
+                  auth.signOut(() => navigate("/sign-in"));
+                }}
+              >
+                <FontAwesomeIcon icon={faUser} />
+                {auth.user.firstName}
+              </button>
+            </Link>
+          ) : (
+            <Link className="App-login" to="/sign-in">
+              <FontAwesomeIcon icon={faRightToBracket} />
+              Log In
+            </Link>
+          )}
+        </div>
+      </header>
+      <p style={{ "font-size": "10px" }}>v.1.0</p>
+    </>
   );
 };
 
