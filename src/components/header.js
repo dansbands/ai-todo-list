060bbd98ea921d21f71a1ec830a2a24676187f91
dansbands <danodeawebdev@gmail.com>
2025-05-19 14:01:30 -0400

    fix row width on safari

--- src/components/header.js
+++ src/components/header.js
@@ -38,7 +38,7 @@ const Header = ({ isLight }) => {
           )}
         </div>
       </header>
-      <p style={{ "font-size": "10px" }}>v.1.2</p>
+      <p style={{ "font-size": "10px" }}>v.1.3</p>
     </>
   );
 };
