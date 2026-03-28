cedc8d1c616ea47600261aed134035da699400ef
dansbands <danodeawebdev@gmail.com>
2026-03-28 14:48:42 -0400

    Remove task input label

--- src/pages/input.js
+++ src/pages/input.js
@@ -29,10 +29,10 @@ const Input = ({
 
   return (
     <div className="form-input-group">
-      <label htmlFor={name}>{title}</label>
       <input
         className="form-input"
         name={name}
+        placeholder={title}
         value={inputValue}
         onChange={handleInputChange}
         onKeyUp={checkForSubmit}
